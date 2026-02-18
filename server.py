52542c250c7665c6f7aea5d8054d8a5b036c7c0b
EunikeObi <s160424019@student.ubaya.ac.id>
2026-02-18 06:20:33 -0500

    modified server.py to match pylint score

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
+"""
+Flask server for Emotion Detection application.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection import emotion_detector
 
 app = Flask("Emotion Detection")
 
 @app.route('/emotionDetector')
-def Main():
+def main():
+    """Analyze text and return detected emotions."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     anger = response['anger']
@@ -14,14 +18,17 @@ def Main():
     sadness = response['sadness']
     dominant = response['dominant_emotion']
 
-    if dominant == None:
+    if dominant is None:
         return_text = "Invalid text! Please try again!"
     else:
-        return_text = f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. The dominant emotion is {dominant}"
-    return return_text 
+        return_text = (f"For the given statement, the system response is 'anger': {anger},"
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}."
+        f" The dominant emotion is {dominant}")
+    return return_text
 
 @app.route("/")
 def render_index_page():
+    """Render the main index page."""
     return render_template('index.html')
 
 if __name__ == "__main__":
